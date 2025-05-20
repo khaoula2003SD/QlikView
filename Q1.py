ef0478e068a01da3454d5e8af7257c3daf39c5db
khaoula2003SD <145676456+khaoula2003SD@users.noreply.github.com>
2025-05-20 09:54:14 +0100

    Q1.py

--- Q1.py
+++ Q1.py
@@ -2,7 +2,7 @@
  "cells": [
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": None,
    "id": "799b98ce-b59e-4e53-b0f5-3b84a4509b18",
    "metadata": {},
    "outputs": [],
